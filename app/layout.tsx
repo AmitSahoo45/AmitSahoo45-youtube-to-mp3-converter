af4df272a695f1a752d2e865b25e6d8e1cf895bb
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2023-12-13 00:00:52 +0530

    Added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   )
 }
